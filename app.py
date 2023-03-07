b3e0399aa3195d6ef5c7b4cae8a29fe79b5fba03
Ryli Botts <rdbotts@iu.edu>
2023-03-06 20:14:53 -0500

    add-dino-helper was added

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 import csv
 
 app = Flask(__name__)
 
 DINO_PATH = app.root_path + '/dinosaurs.csv'
 FAQ_PATH = app.root_path + '/faq.csv'
+DINO_KEYS = ['slug', 'name', 'description', 'image', 'image-credit', 'source-url', 'source-credit']
 
 
 def get_dinos():
-    with open(DINO_PATH, 'r') as csvfile:
-        data = csv.DictReader(csvfile)
-        dinosaurs = {row['slug']:{'name':row['name'], 'description':row['description'], 'image':row['image'], 'image-credit':row['image-credit'], 'source-url':row['source-url'], 'source-credit':row['source-credit']} for row in data}
-    
+    try:
+        with open(DINO_PATH, 'r') as csvfile:
+            data = csv.DictReader(csvfile)
+            dinosaurs = {}
+            for dino in data:
+                dinosaurs[dino['slug']] = dino
+    except Exception as e:
+        print(e)
     return dinosaurs
 # with open('dinosaurs.csv', 'r') as csvfile:
 #     data = csv.DictReader(csvfile)
 #     dinosaurs = {row['slug']:{'name':row['name'], 'description':row['description'], 
 # 'image':row['image'], 'image-credit':row['image-credit'], 'source-url':row['source-url'], 
 # 'source-credit':row['source-credit']} for row in data}
+
+def set_dinos(dinosaurs):
+    try:
+        with open(DINO_PATH, mode='w', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=DINO_KEYS)
+            writer.writeheader()
+            for dino in dinosaurs.values():
+                writer.writerow(dino)
+    except Exception as err:
+        print(err)
+
 
 # @app.route('/')
 # def index():
@@ -50,3 +66,27 @@ def faq():
     faq = get_faq()
     return render_template('faq.html', faq_list=faq)
  
+@app.route('/add-dino', methods = ['GET', 'POST'])
+def add_dino():
+    if request.method == 'POST':
+    #grab te user data from POST
+        in_slug = request.form['slug']
+        in_description = request.form['description']
+        in_name = request.form['name']
+        in_image = request.form['image']
+        in_image_credit= request.form['image-credit']
+        in_source_url = request.form['source-url']
+        in_source_credit = request.form['source-credit']
+    #process the data 
+    #create a new directory with the new dinosaur data in it
+        new_dino = {'slug':in_slug, 'name':in_name, 'description':in_description, 'image':in_image, 'image-credit':in_image_credit, 'source-url':in_source_url, 'source-credit':in_source_credit}
+
+        #grab the existing dinosaur data and add new dino
+        dinosaurs=get_dinos()
+        dinosaurs[in_slug] = new_dino
+
+        set_dinos(dinosaurs)
+
+        # return redirect(url_for('index'))
+    else:
+        return render_template('add-dino.html')
